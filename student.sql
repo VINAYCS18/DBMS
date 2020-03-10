ecef7a7e2af2177276510b5ff0ec2053b359039d
vinayte18 <57659769+vinayte18@users.noreply.github.com>
2020-03-11 01:05:47 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -40,17 +40,42 @@ CREATE TABLE ENROLL(
     FOREIGN KEY(COURSE_ID) REFERENCES COURSE(COURSE_ID)
     );
     
-INSERT INTO STUDENT VALUES('&REGNO', '&NAME', '&MAJOR', '&DATE');
-INSERT INTO COURSE VALUES(&COURSE_ID, '&CNAME', '&DEPT');
-INSERT INTO TEXT VALUES(&ISBN, '&BOOK_TITLE', '&PUBLISHER', '&AUTHOR');
-INSERT INTO ADOPTION VALUES(&COURSE_ID, &SEM, &ISBN);
-INSERT INTO ENROLL VALUES('&REGNO', &COURSE_ID, &SEM, &MARKS);
+insert into student values ('1DB01IS01', 'KIRAN', 'ISE', '02-JAN-84');
+insert into student values ('1DB01CS02', 'DIVYA', 'CSE', '12-OCT-85');
+insert into student values ('1DBO1ME03', 'BABU', 'MECH', '23-SEP-82');
+insert into student values ('1DB01EC04', 'JOHN', 'EC', '21-FEB-84');
+insert into student values ('1DB01CS05', 'ASHA', 'CSE', '07-OCT-79');
+select * from student;
+insert INTO course values (11, 'DCS', 'CSE');
+insert INTO course values (22, 'ADA', 'CSE');
+insert INTO course values (33, 'CN', 'EC');
+insert INTO course values (44, 'TD' ,'MECH');
+insert INTO course values (55 ,'MUP', 'EC');
+select * from course;
+insert INTO text values (7722, 'VB', 'PHI', 'HOLZNER');
+insert INTO text values (1144, 'DS WITH C', 'SAPNA', 'NANDAGOPAL');
+insert INTO text values (4400, 'C PROG', 'PHI', 'BALAGURU');
+insert INTO text values (5566, 'CN', 'PHI', 'TENENBAUM');
+insert INTO text values ('3388', 'MP', 'TATA', 'BREY');
+select * from text;
 
-SELECT * FROM STUDENT;
-SELECT * FROM COURSE;
-SELECT * FROM TEXT;
-SELECT * FROM ADOPTION;
-SELECT * FROM ENROLL;
+insert INTO BOOK_ADOPTION values (11, 3, 7722);
+insert INTO BOOK_ADOPTION values (22, 4, 7722);
+insert INTO BOOK_ADOPTION values (11, 5, 4400);
+insert INTO BOOK_ADOPTION values (11, 8, 5566);
+insert INTO BOOK_ADOPTION values (55, 4, 3388);
+insert INTO BOOK_ADOPTION values (44, 4, 5566);
+insert INTO BOOK_ADOPTION values (44, 7, 3388);
+select * from BOOK_Adoption;
+
+
+insert INTO enroll values ('1DB01IS01', 22, 5, 76);
+insert INTO enroll values ('1DB01IS01', 11, 3, 90);
+insert INTO enroll values ('1DB01CS02', 33, 6, 55);
+insert INTO enroll values ('1DBO1ME03', 22, 5, 75);
+insert INTO enroll values ('1DB01CS05', 44, 5, 75);
+select * from enroll;
+
 
 INSERT INTO TEXT VALUES('4242', 'PYTHON PROG', 'PHI', 'DAVE R');
 INSERT INTO ADOPTION VALUES(40, 4, 4242);
@@ -63,4 +88,4 @@ ORDER BY C.CNAME;
 
 SELECT C.CNAME 
 FROM COURSE C, ADOPTION A, TEXT T 
-WHERE T.ISBN = A.ISBN AND C.COURSE_ID = A.COURSE_ID AND ;
+WHERE T.ISBN = A.ISBN AND C.COURSE_ID = A.COURSE_ID  ;
